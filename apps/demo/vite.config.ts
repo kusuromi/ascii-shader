a85a4c086c844693ca963cf76af45f120b51a050
kusuromi <kusuromi@gmail.com>
2026-09-17 11:52:56 +0300

    Deploy demo to GitHub Pages

--- apps/demo/vite.config.ts
+++ apps/demo/vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const isPagesDeploy = process.env.GITHUB_ACTIONS === "true";
+
 export default defineConfig({
+  base: isPagesDeploy ? "/ascii-background/" : "/",
   plugins: [react()],
 });
